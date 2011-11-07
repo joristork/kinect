3b425d4689fe01a7fdc262a9ddc21d49c7414f6c
Joris Stork <joris@wintermute.eu>
2011-11-07 13:27:34 +0100

    added a project plan

--- src/read_kinect.py
+++ src/read_kinect.py
@@ -16,17 +16,40 @@ import matplotlib.image as mpimg
 import cv
 import numpy as np
 import freenect
+import frame_convert
 
 def main():
+    """ 
+    note: the get_video and get_depth functions in freenect return an image plus
+    a timestamp
+    
+    """
     raw_input('\nLet\'s view an rgb image from the kinect:\n(Press enter)')
-    try:
-        depth, rgb = freenect.sync_get_depth(), freenect.sync_get_video()
-        plt.subplot(1,1,1)
-        plt.imshow(np.flipud(rgb))
-        plt.show()
-    except:
-        print '\nHum, something went wrong. Try again...'
-        main()
+    while 1:
+        print 'loop de loop'
+        try:
+            print 'checkpoint 1'
+            depth = freenect.sync_get_depth()[0]
+            print 'checkpoint 5'
+            plt.subplot(1,2,2)
+            print 'checkpoint 6'
+            plt.imshow(np.flipud(depth))
+            print 'checkpoint 7'
+            
+            rgb = freenect.sync_get_video()[0]
+            print 'checkpoint 2'
+            plt.subplot(1,1,1)
+            print 'checkpoint 3'
+            plt.imshow(np.flipud(rgb))
+            print 'checkpoint 4'
+
+
+            plt.show()
+            print 'checkpoint 8'
+
+        except:
+            print '\nHum, something went wrong. Try again...'
+            main()
 
 
 if __name__ == "__main__":
