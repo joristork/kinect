d9ada9c245dfff08111a23b12ff3bfe7708d610a
Jimi van der Woning <jimi@jimi-laptop.(none)>
2011-11-11 09:32:14 +0100

    Read kinect

--- src/read_kinect.py
+++ src/read_kinect.py
@@ -1,3 +1,34 @@
+"""
+#!/usr/bin/env python
+import sys
+sys.path.append("/usr/local/lib/python2.6/site-packages")
+
+import freenect
+import cv
+#import frame_convert
+
+cv.NamedWindow('Depth')
+cv.NamedWindow('Video')
+print('Press ESC in window to stop')
+
+
+def get_depth():
+    return freenect.sync_get_depth()[0]
+    #return frame_convert.pretty_depth_cv(freenect.sync_get_depth()[0])
+
+
+def get_video():
+    return freenect.sync_get_video()[0]
+    #return frame_convert.video_cv(freenect.sync_get_video()[0])
+
+
+while True:
+    cv.ShowImage('Depth', get_depth())
+    cv.ShowImage('Video', get_video())
+    if cv.WaitKey(10) == 27:
+        break
+"""
+
 #!/usr/bin/env python
 """
 :synopsis:  Kinect project for part 2 of Beeldbewerken course, BSc
@@ -8,15 +39,16 @@
 .. moduleauthor:: Joris Stork <joris@wintermute.eu>
 
 """
-
 __author__ = "Joris Stork"
 
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import cv
+try:
+    import cv
+except:
+    import opencv
 import numpy as np
 import freenect
-import frame_convert
+#import frame_convert
 
 def main():
     """ 
@@ -25,32 +57,26 @@ def main():
     
     """
     raw_input('\nLet\'s view an rgb image from the kinect:\n(Press enter)')
-    while 1:
-        print 'loop de loop'
-        try:
-            print 'checkpoint 1'
-            depth = freenect.sync_get_depth()[0]
-            print 'checkpoint 5'
-            plt.subplot(1,2,2)
-            print 'checkpoint 6'
-            plt.imshow(np.flipud(depth))
-            print 'checkpoint 7'
-            
-            rgb = freenect.sync_get_video()[0]
-            print 'checkpoint 2'
-            plt.subplot(1,1,1)
-            print 'checkpoint 3'
-            plt.imshow(np.flipud(rgb))
-            print 'checkpoint 4'
 
+    i = 0
+   # while 1:
+##        print 'loop de loop'
+##    i = i+1
+##    if i%10 == 0:
+    try:
+        depth = freenect.sync_get_depth()[0]
+##            print 'depth:', depth
+        plt.imshow(np.flipud(depth), origin="bottom")
+        #plt.draw()
+        
+##            rgb = freenect.sync_get_video()[0]
+##            print 'rgb:', rgb
+##            plt.subplot(1,2,2)
+##            plt.imshow(np.flipud(rgb), origin="bottom")
 
-            plt.show()
-            print 'checkpoint 8'
-
-        except:
-            print '\nHum, something went wrong. Try again...'
-            main()
-
+    except:
+        print '\nHum, something went wrong. Try again...'
+    plt.show()
 
 if __name__ == "__main__":
     print '\n*** Test application to read and display Kinect data ***\n'
