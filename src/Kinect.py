cc7da3fbdadad97c85e6597ac575a56b888083e8
holbewoner <luuk@noregular.com>
2011-12-12 19:53:35 +0100

    Added description on how to use Kinect.py

--- src/Kinect.py
+++ src/Kinect.py
@@ -4,6 +4,45 @@ import calibrate as calib
 import sys
 import cv
 
+
+"""
+This module combines all functions. It can be called with:
+
+./Kinect 
+./Kinect.py 0       
+    Use the Kinect for making a 3d model
+    
+./Kinect.py 1
+    The program starts using a static images for making a 3d model, so that 
+    you can work without a Kinect. You don't have to select all the points in the
+    image either, they are preset. If you do want to specify the points 
+    yourself, pass any argument BUT 0 and 1. 
+
+./Kinect.py * 
+    The program starts using a static image for making a 3d model, so that 
+    we can work without a Kinect. 
+    
+./Kinect.py * A
+    The value of A specifies the index of the camera that is to be used for
+    calibration. If -1 is passed, the program chooses itself. If two camera's
+    are attached (e.g. a camera in your laptop and the Kinect) they likely have 
+    the indexes 0 and 1.
+    
+./Kinect * A 1
+    The programs has a module that calculates the intrinsic camera matrix. This
+    has to be done once for a camera, so instead of doing it over all the time
+    you load the intrinsic camera matrix from a xml file if the 3rd argument
+    is "1". 
+    
+./Kinect * A 2
+    If a new camera is used, you have to determine the new camera matrix. This
+    can be done each time, but it is more convenient to store the new matrix
+    in a xml file so that it can be loaded next time you execute the program
+    (by setting the 3rd argument to "1"). You can set the program in "save" mode
+    by setting the 3rd argument to "2". 
+
+"""
+
 def menu(argv):
     """ The main menu. ``fails''= number of invalid choices """
     fails = 0
