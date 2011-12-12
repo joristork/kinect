3898585abb784896109e87f693904212a4878baa
holbewoner <luuk@noregular.com>
2011-12-12 02:14:23 +0100

    Calibration intrinsic matrix done, almost there

---
+++ src/Kinect.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import create_model as cm
+import calibrate as calib
+import sys
+import cv
+
+def menu(argv):
+    """ The main menu. ``fails''= number of invalid choices """
+    fails = 0
+    i_c = None
+    points = None    
+    
+    def prompt(fails, i_c, points, argv):
+        """Note that we ensure ``choice'' is not interpreted as a string"""
+        cv.NamedWindow("test")
+        cv.DestroyAllWindows()
+        print '\n --- MAIN MENU ---'
+        print '\n [1] Calibrate the camera'
+        print '\n [2] Start making a 3d model'
+        print '\n [3] Exit'        
+        choice = 0
+        choice = raw_input('\nChoose one:\n>> ')
+        router(choice, fails, i_c, points, argv)
+
+    def router(choice, fails, i_c, points, argv):
+        """ Executes the desired choice, if it is valid """
+        if choice == '1':
+            print "\nStart to calibrate the camera\n" 
+            i_c = calib.get_intrinsic_matrix()
+            prompt(0,i_c, points, argv)
+        elif choice == '2':
+            print "\nStart generating modelpoints\n"
+            points = cm.generate_modelpoints(i_c, argv)
+            prompt(0,i_c, points, argv)
+        elif choice == '3':
+            print '\nGoodbye!\n'
+            sys.exit(0)
+        else:
+            tryagain(fails, choice, i_c, points, argv)
+
+    def tryagain(fails, choice, i_c, points, argv):
+        """ Gives the user three chances to enter a valid choice """
+        fails += 1
+        if (fails > 2):
+            print '\nToo many wrong choices. Exiting...\n'
+            sys.exit(0)
+        print '\nSorry, ``'+choice+'\'\' is not a valid choice. Try again.'
+        prompt(fails, i_c, points, argv)
+    prompt(fails, i_c, points, argv)
+
+
+
+if __name__ == '__main__':
+    menu(sys.argv)
---
+++ src/calibrate.py
@@ -0,0 +1,170 @@
+import exceptions
+import cv
+import numpy as np
+import time
+
+enable_capture = False
+captured_points = []
+clicks = 0
+quit = False
+current_points = []
+accept = False
+
+def get_intrinsic_matrix():
+    """Note that we ensure ``choice'' is not interpreted as a string"""
+    global enable_capture, quit, current_points, captured_points, accept
+    enable_capture = False
+    captured_points = []
+    clicks = 0
+    quit = False
+    current_points = []
+    accept = False    
+    
+    cb_dim = get_cb_dimensions()
+
+    
+    
+    if cb_dim == None:
+       return None
+
+    cam = cv.CaptureFromCAM(-1);
+    cv.NamedWindow('Calibrate')
+    cv.SetMouseCallback("Calibrate",mouseclick,None)
+    font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.5, 0.5, 0, 1, 8)
+
+    
+    while True:
+        image = cv.QueryFrame(cam)
+        image_size = cv.GetSize(image)
+        
+        grey_image = cv.CreateImage(image_size, cv.IPL_DEPTH_8U, 1 ) 
+        cv.CvtColor(image,grey_image,cv.CV_BGR2GRAY)
+        found, points = cv.FindChessboardCorners(grey_image, cb_dim, cv.CV_CALIB_CB_ADAPTIVE_THRESH)
+        
+        
+        if found:
+            points = cv.FindCornerSubPix(grey_image, points, (11,11), (-1,-1), (cv.CV_TERMCRIT_EPS+cv.CV_TERMCRIT_ITER,30,0.1)) 	
+            cv.DrawChessboardCorners(image, cb_dim, points, found)
+            cv.PutText(image,"Use left-click to capture points",(2, 40), font, (0,0,0))  
+            current_points = points
+            enable_capture = True
+        else:
+            enable_capture = False
+            
+            
+        if quit:
+            break
+            
+            
+        cv.PutText(image,"Click twice on the right mousebutton to finalize the capture.",(2, 20), font, (0,0,0))            
+        cv.ShowImage("Calibrate", image)
+
+        key = ( cv.WaitKey(1) ) % 0x100
+        if key == 27:
+            break
+    
+    cv.DestroyWindow("Calibrate")
+    
+    if len(captured_points) > 0:
+        number_of_points = cb_dim[0]*cb_dim[1]
+       
+        
+        shots = len(captured_points) / number_of_points
+        pointcounts = cv.CreateMat(shots, 1, cv.CV_32SC1)
+        for i in xrange(shots):
+            pointcounts[i,0] = number_of_points
+
+        camera_matrix = cv.CreateMat(3, 3, cv.CV_32FC1)
+        distortion = cv.CreateMat(5, 1, cv.CV_32FC1)
+        rvecs = cv.CreateMat(shots, 3, cv.CV_32FC1)
+        tvecs = cv.CreateMat(shots, 3, cv.CV_32FC1)
+        
+        cv.Set2D(camera_matrix,0,0,1.0)
+        cv.Set2D(camera_matrix,1,1,1.0)
+        
+        
+        o_points = cv.CreateMat(number_of_points * shots, 3, cv.CV_32FC1)
+        i_points = cv.CreateMat(number_of_points * shots, 3, cv.CV_32FC1)
+        j = -1
+        for i in xrange(number_of_points * shots):
+            if i % cb_dim[1] == 0: 
+                j = (j + 1)
+            o_points[i, 0] = j % cb_dim[0]
+            o_points[i, 1] = i % cb_dim[1]
+            
+            o_points[i, 2] = 0.0
+            i_points[i, 0] = captured_points[i][0]
+            i_points[i, 1] = captured_points[i][1]
+
+        print "Calculating intrinsic matrix....."
+        cv.CalibrateCamera2(o_points, i_points, pointcounts, image_size, camera_matrix, distortion, rvecs, tvecs) 
+        print "Done"
+        
+        
+
+        cv.NamedWindow( "Undistort" )
+        cv.SetMouseCallback("Undistort",mouseclick_dist,None)
+
+
+        print "Show the resulting camera image. Left click to accept."
+        while(1):
+            image=cv.QueryFrame(cam)
+            t = cv.CloneImage(image) 
+            cv.Undistort2(image, t, camera_matrix, distortion)
+            cv.ShowImage("Undistort", t)
+            c = cv.WaitKey(33)
+            if accept:
+                break
+            elif c==1048603:		# enter esc key to exit
+                break
+                
+                
+        cv.DestroyAllWindows() 
+        print "\nInfo: Intrinsic matrix and distortion coefficients have been set."
+        return camera_matrix, distortion
+    else:
+        return None
+
+def mouseclick_dist(event,x,y,flags,param):
+    global accept
+    if event == cv.CV_EVENT_LBUTTONUP:
+        accept = True
+    
+    
+def mouseclick(event,x,y,flags,param):
+    global enable_capture, captured_points, clicks, quit, current_points
+    if event == cv.CV_EVENT_LBUTTONUP:
+        clicks = 0
+        if enable_capture:
+            print "Added new points"
+            captured_points = captured_points + current_points
+    if event == cv.CV_EVENT_RBUTTONUP:
+        if clicks == 0:
+            clicks = 1
+        else:
+            quit = True
+
+    
+def get_cb_dimensions():
+    CB_width = 0
+    CB_width = raw_input('\nChessboard width:\n>> ')
+    CB_height = 0
+    CB_height = raw_input('\nChessboard height:\n>> ')
+    
+    try:
+        CB_width = int(CB_width)
+        CB_height = int(CB_height) 
+        return (CB_width, CB_height)       
+    except exceptions.ValueError:
+        print "You gave invalid values for the dimensions. Please press enter to try again or press 'x' and enter to go back to the main menu."
+        inp = raw_input(">> ")
+        while(inp != "x" and inp != ""):
+            inp = raw_input(">> ")
+        if inp == "":
+            return get_cb_dimensions()
+    return None
+    
+if __name__ == '__main__':
+    matrix, distortion =  get_intrinsic_matrix()
+    for i in xrange(3):
+       print matrix[i,0],"\t\t", matrix[i,1],"\t\t", matrix[i,2]
